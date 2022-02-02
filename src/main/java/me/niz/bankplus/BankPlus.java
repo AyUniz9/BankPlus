2cce37ee97f30737ef08be23fd9a8192cfefb250
BuildTools <unconfigured@null.spigotmc.org>
2022-02-02 21:45:52 +0100

    Initial commit

---
+++ src/main/java/me/niz/bankplus/BankPlus.java
@@ -0,0 +1,17 @@
+package me.niz.bankplus;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BankPlus extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
